ae4819370245a46722aa246feafad961ec7c1f70
[SW2/서울/2반] 김태훈 <kth5954@gmail.com>
2022-10-19 01:19:16 +0900

    ch2: add AND Gate

---
+++ main.py
@@ -0,0 +1,12 @@
+import Gate
+
+def main():
+    print("Gate.AND(0, 0): ", Gate.AND(0, 0))
+    print("Gate.AND(0, 1): ", Gate.AND(0, 1))
+    print("Gate.AND(1, 0): ", Gate.AND(1, 0))
+    print("Gate.AND(1, 1): ", Gate.AND(1, 1))
+
+
+if __name__ == "__main__":
+    main()
+
